824975a84f9f66c6d278906e83692898332b97b7
George <backs159@icloud.com>
2022-01-19 00:19:40 +0500

    save before going to bed

--- LogoPassChecker/LoginViewController.swift
+++ LogoPassChecker/LoginViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var forgotPassButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
